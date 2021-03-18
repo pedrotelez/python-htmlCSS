f4cb140c286c6e24dd3ef488d5476bd6ee97efea
pedrotelez <pedroptelez@gmail.com>
2021-03-18 23:42:08 +0000

    parte2

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,47 @@
-from app import app
-from flask import render_template, request
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 
-@app.route('/')    # cria uma rota default
+@app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Pedro'}
-    title = "Homepage"
+    user = {'username': 'Pedro'}
     posts = [
         {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
-        {'author': {'username': 'Pedro'}, 'body': "Olá!"}
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
     ]
-    return render_template("index.html", user=user, posts=posts, title=title)   # renderiza um arquivo html
+    return render_template("index.html", user=user, posts=posts)
 
-@app.route('/login', methods=["GET", "POST"]) 
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     if request.method == "POST":
-        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
-    return render_template("login.html", title="Login")
+        user = User.query.filter_by(username=request.values.get("user")).first()
+        if user is None or not user.password == request.values.get("pass"):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        r = request.values.get("remember") == "on"
+        login_user(user, remember=r)
+        return redirect(url_for('index'))
+    return render_template("login.html" , title="Login")
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
-
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        user = User(username=request.values.get('user'),
+                    password=request.values.get('pass'))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html" , title="Register")
+ 
