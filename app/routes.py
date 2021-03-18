3d99c52f561b590469f7f39c6a7579c1e4321016
pedrotelez <pedroptelez@gmail.com>
2021-03-18 20:02:59 +0000

    parte1

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,15 @@ from app import app
 from flask import render_template
 
 
-@app.route('/')
+@app.route('/')    # cria uma rota default
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    user = {'username' : 'Pedro'}
+    title = "Homepage"
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Pedro'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts, title=title)   # renderiza um arquivo html
 
 
